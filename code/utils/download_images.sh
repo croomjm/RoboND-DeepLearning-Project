7cf6a8f99e9d10f38d693f19934aa6696a12bf17
croomjm <croomjm@gmail.com>
2017-11-15 22:22:52 -0800

    corrected unzip locaiton

--- code/utils/download_images.sh
+++ code/utils/download_images.sh
@@ -3,8 +3,8 @@ wget -P ~/RoboND-DeepLearning-Project/data/ https://s3-us-west-1.amazonaws.com/u
 wget -P ~/RoboND-DeepLearning-Project/data/ https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Project/sample_evaluation_data.zip
 
 unzip ~/RoboND-DeepLearning-Project/data/train.zip -d ~/RoboND-DeepLearning-Project/data
-unzip ~/RoboND-DeepLearning-Project/data/validation.zip ~/RoboND-DeepLearning-Project/data
-unzip ~/RoboND-DeepLearning-Project/data/sample_evaluation_data.zip ~/RoboND-DeepLearning-Project/data
+unzip ~/RoboND-DeepLearning-Project/data/validation.zip -d ~/RoboND-DeepLearning-Project/data
+unzip ~/RoboND-DeepLearning-Project/data/sample_evaluation_data.zip -d ~/RoboND-DeepLearning-Project/data
 
 rm ~/RoboND-DeepLearning-Project/data/*.zip
 
