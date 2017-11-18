7c071a620d34771c8bd5e183f635d17f4cdc2d13
croomjm <croomjm@gmail.com>
2017-11-18 13:07:28 -0800

    Updated to pull in additional training data if requested.

--- code/utils/download_images.sh
+++ code/utils/download_images.sh
@@ -6,6 +6,18 @@ unzip ~/RoboND-DeepLearning-Project/data/train.zip -d ~/RoboND-DeepLearning-Proj
 unzip ~/RoboND-DeepLearning-Project/data/validation.zip -d ~/RoboND-DeepLearning-Project/data
 unzip ~/RoboND-DeepLearning-Project/data/sample_evaluation_data.zip -d ~/RoboND-DeepLearning-Project/data
 
-rm ~/RoboND-DeepLearning-Project/data/*.zip
+rm ~/RoboND-DeepLearning-Project/data/train.zip
+rm ~/RoboND-DeepLearning-Project/data/validation.zip 
+rm ~/RoboND-DeepLearning-Project/data/sample_evaluation_data.zip
 
 mv ~/RoboND-DeepLearning-Project/data/train_combined ~/RoboND-DeepLearning-Project/data/train
+
+echo "Do you wish to download supplemental training data?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) ./~/RoboND-DeepLearning-Project/code/utils/Robdownload_supplemental_training_data.sh; break;;
+        No ) exit;;
+    esac
+done
+
+
