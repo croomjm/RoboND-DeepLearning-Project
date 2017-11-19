a57de41614118ae7e82bf5b22faa1d5b18d6dfd6
croomjm <croomjm@gmail.com>
2017-11-18 16:12:30 -0800

    added step to remove empty additional_train folder after combining with the rest of the training set.

--- code/utils/download_supplemental_training_data.sh
+++ code/utils/download_supplemental_training_data.sh
@@ -1,8 +1,11 @@
 wget -P ~/RoboND-DeepLearning-Project/data/ https://s3-us-west-2.amazonaws.com/robond-deep-learn-project-training-data/additional_train.zip
 
+https://s3-us-west-2.amazonaws.com/robond-deep-learn-project-training-data/additional_train.zip
+
 unzip ~/RoboND-DeepLearning-Project/data/additional_train.zip -d ~/RoboND-DeepLearning-Project/data
 
 rm ~/RoboND-DeepLearning-Project/data/*.zip
 
 mv ~/RoboND-DeepLearning-Project/data/additional_train/images/* ~/RoboND-DeepLearning-Project/data/train/images/
 mv ~/RoboND-DeepLearning-Project/data/additional_train/masks/* ~/RoboND-DeepLearning-Project/data/train/masks/
+rm -r ~/RoboND-DeepLearning-Project/data/additional_train/
