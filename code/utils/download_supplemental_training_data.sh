487b12e8336ad3b14b9809d44e11f28b8bea834f
croomjm <croomjm@gmail.com>
2017-11-19 20:26:51 -0800

    added check for total number of images

--- code/utils/download_supplemental_training_data.sh
+++ code/utils/download_supplemental_training_data.sh
@@ -6,9 +6,21 @@ unzip ~/RoboND-DeepLearning-Project/data/additional_train2.zip -d ~/RoboND-DeepL
 
 rm ~/RoboND-DeepLearning-Project/data/*.zip
 
+echo "Number of files in train folder:"
+ls ~/RoboND-DeepLearning-Project/data/train/images/ -1 | wc -l
+
+echo "Number of files in additional_train folder:"
+ls ~/RoboND-DeepLearning-Project/data/additional_train/images/ -1 | wc -l
+
+echo "Number of files in additional_train2 folder:"
+ls ~/RoboND-DeepLearning-Project/data/additional_train2/images/ -1 | wc -l
+
 mv ~/RoboND-DeepLearning-Project/data/additional_train/images/* ~/RoboND-DeepLearning-Project/data/train/images/
 mv ~/RoboND-DeepLearning-Project/data/additional_train/masks/* ~/RoboND-DeepLearning-Project/data/train/masks/
 mv ~/RoboND-DeepLearning-Project/data/additional_train2/images/* ~/RoboND-DeepLearning-Project/data/train/images/
 mv ~/RoboND-DeepLearning-Project/data/additional_train2/masks/* ~/RoboND-DeepLearning-Project/data/train/masks/
 rm -r ~/RoboND-DeepLearning-Project/data/additional_train/
 rm -r ~/RoboND-DeepLearning-Project/data/additional_train2/
+
+echo "Number of files in train folder:"
+ls ~/RoboND-DeepLearning-Project/data/train/images/ -1 | wc -l
